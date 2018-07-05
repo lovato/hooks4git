d66a7f40b1afeb79c8cbd8a5c4566b0dccec02d5
Marco Lovato <maglovato@gmail.com>
2018-07-05 09:20:57 -0300

    additional files

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+# pep8: disable-msg=E501
+# pylint: disable=C0301
+import os
+import logging
+import getpass
+import tempfile
+
+__version__ = '0.1'
+__author__ = 'Marco Lovato'
+__author_username__ = 'lovato'
+__author_email__ = 'maglovato@gmail.com'
+__description__ = 'Extensible Hook System for GIT'
+
+log_filename = os.path.join(tempfile.gettempdir(),
+                            'hooks4git-' + getpass.getuser() + '.log')
+
+log = logging
+log.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(levelname)s %(message)s',
+                filename=log_filename,
+                filemode='a')
+
+
+def __path(filename):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+
+
+# Travis
+if os.getenv("TRAVIS_BUILD_NUMBER"):
+    file_ = open(__path('build.info'), 'w')
+    file_.write(os.getenv("TRAVIS_BUILD_NUMBER"))
+    file_.close()
+
+__build__ = '0'
+if os.path.exists(__path('build.info')):
+    __build__ = open(__path('build.info')).read().strip()
+
+__version__ = __version__ + '.' + __build__
--- setup.py
+++ setup.py
@@ -18,16 +18,24 @@ with codecs.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 with codecs.open(path.join(here, 'requirements-dev.txt'), encoding='utf-8') as f:
     requirements_dev = f.read()
 
+project_name = 'hooks4git'
+__version__ = __import__(project_name).__version__
+__author__ = __import__(project_name).__author__
+__author_email__ = __import__(project_name).__author_email__
+__author_username__ = __import__(project_name).__author_username__
+__description__ = __import__(project_name).__description__
+
 setup(
-    name="hooks4git",
-    version="0.1.0",
-    author="Marco Lovato (based on https://github.com/collin5/precommit-hook)",
-    author_email="maglovato@gmail.com",
+    name=project_name,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    download_url='https://github.com/{}/{}/tarball/{}'.format(__author_username__, project_name, __version__),
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
     url="https://github.com/lovato/hooks4git",
-    description="Extensible Hook System for GIT",
+    description=__description__,
     long_description=long_description,
     long_description_content_type='text/markdown',
     platforms=['any'],
@@ -37,5 +45,6 @@ setup(
     scripts=["app/precommit.hook"],
     cmdclass={
             "install": Post_install,
-    }
+    },
+    zip_safe=True
 )
