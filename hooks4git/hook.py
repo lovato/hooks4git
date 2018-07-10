fc930c3ae73fbfc195361ffc8d0e4810675e321a
Marco Lovato <maglovato@gmail.com>
2018-07-10 20:50:52 -0300

    adding colorama ansi

--- hooks4git/hook.py
+++ hooks4git/hook.py
@@ -4,10 +4,125 @@ import os
 import subprocess
 import sys
 import configparser
-from colorama import Fore
-from colorama import Back
-from colorama import Style
 import datetime
+
+# *****************************************************************************
+# https://github.com/tartley/colorama/blob/83364bf1dc2bd5a53ca9bd0154fe21d769d6f90f/colorama/ansi.py
+#
+# THIS FILE WAS MODIFIED FROM ORIGINAL
+#
+# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
+'''
+This module generates ANSI character codes to printing colors to terminals.
+See: http://en.wikipedia.org/wiki/ANSI_escape_code
+'''
+
+CSI = '\033['
+OSC = '\033]'
+BEL = '\007'
+
+
+def code_to_chars(code):
+    return CSI + str(code) + 'm'
+
+
+def set_title(title):
+    return OSC + '2;' + title + BEL
+
+
+def clear_screen(mode=2):
+    return CSI + str(mode) + 'J'
+
+
+def clear_line(mode=2):
+    return CSI + str(mode) + 'K'
+
+
+class AnsiCodes(object):
+    def __init__(self):
+        # the subclasses declare class attributes which are numbers.
+        # Upon instantiation we define instance attributes, which are the same
+        # as the class attributes but wrapped with the ANSI escape sequence
+        for name in dir(self):
+            if not name.startswith('_'):
+                value = getattr(self, name)
+                setattr(self, name, code_to_chars(value))
+
+
+class AnsiCursor(object):
+    def UP(self, n=1):
+        return CSI + str(n) + 'A'
+
+    def DOWN(self, n=1):
+        return CSI + str(n) + 'B'
+
+    def FORWARD(self, n=1):
+        return CSI + str(n) + 'C'
+
+    def BACK(self, n=1):
+        return CSI + str(n) + 'D'
+
+    def POS(self, x=1, y=1):
+        return CSI + str(y) + ';' + str(x) + 'H'
+
+
+class AnsiFore(AnsiCodes):
+    BLACK = 30
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+    MAGENTA = 35
+    CYAN = 36
+    WHITE = 37
+    RESET = 39
+
+    # These are fairly well supported, but not part of the standard.
+    LIGHTBLACK_EX = 90
+    LIGHTRED_EX = 91
+    LIGHTGREEN_EX = 92
+    LIGHTYELLOW_EX = 93
+    LIGHTBLUE_EX = 94
+    LIGHTMAGENTA_EX = 95
+    LIGHTCYAN_EX = 96
+    LIGHTWHITE_EX = 97
+
+
+class AnsiBack(AnsiCodes):
+    BLACK = 40
+    RED = 41
+    GREEN = 42
+    YELLOW = 43
+    BLUE = 44
+    MAGENTA = 45
+    CYAN = 46
+    WHITE = 47
+    RESET = 49
+
+    # These are fairly well supported, but not part of the standard.
+    LIGHTBLACK_EX = 100
+    LIGHTRED_EX = 101
+    LIGHTGREEN_EX = 102
+    LIGHTYELLOW_EX = 103
+    LIGHTBLUE_EX = 104
+    LIGHTMAGENTA_EX = 105
+    LIGHTCYAN_EX = 106
+    LIGHTWHITE_EX = 107
+
+
+class AnsiStyle(AnsiCodes):
+    BRIGHT = 1
+    DIM = 2
+    NORMAL = 22
+    RESET_ALL = 0
+
+
+Fore = AnsiFore()
+Back = AnsiBack()
+Style = AnsiStyle()
+Cursor = AnsiCursor()
+# *****************************************************************************
+
 
 # from hooks4git import __version__
 __version__ = 0.1
@@ -120,21 +235,21 @@ def main():
             title = "\nhooks4git v%s :: %s :: hook triggered" % (__version__, cmd.title())
             title = Fore.YELLOW + Style.BRIGHT + title + Style.RESET_ALL
             print(title)
-        for command_item in commands:
-            divider()
-            steps_executed += 1
-            files = []
-            # if cmd == 'pre-commit':
-            #     files = get_changed_files()
-            command = scripts[hook[command_item]]
-            result = execute(command.split()[0], files, command.split()[1:])
-            if result[0] != 0:
-                no_fails = False
-                style = Fore.RED + Style.BRIGHT
-                out('FAIL', "%s'%s' step failed to execute ✘ %s" % (style, command.split()[0], Style.RESET_ALL))
-            else:
-                style = Fore.GREEN
-                out('PASS', "%s'%s' step executed successfully ✔ %s" % (style, command.split()[0], Style.RESET_ALL))
+        # for command_item in commands:
+        #     divider()
+        #     steps_executed += 1
+        #     files = []
+        #     # if cmd == 'pre-commit':
+        #     #     files = get_changed_files()
+        #     command = scripts[hook[command_item]]
+        #     result = execute(command.split()[0], files, command.split()[1:])
+        #     if result[0] != 0:
+        #         no_fails = False
+        #         style = Fore.RED + Style.BRIGHT
+        #         out('FAIL', "%s'%s' step failed to execute ✘ %s" % (style, command.split()[0], Style.RESET_ALL))
+        #     else:
+        #         style = Fore.GREEN
+        #         out('PASS', "%s'%s' step executed successfully ✔ %s" % (style, command.split()[0], Style.RESET_ALL))
         return no_fails
     except Exception as e:  # noqa
         out('ERR!', str(e), color=Fore.RED)
