af2b972ba272fabc421dced5d7fdf9aa804d090d
Marco Lovato <maglovato@gmail.com>
2018-08-03 14:27:54 -0300

    adding checkbrach

---
+++ hooks4git/scripts/check_branch_name.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+valid='^(feature|bugfix|hotfix)\/.+'
+branch=`git rev-parse --abbrev-ref HEAD`
+if [[ $branch =~ $valid ]]; then
+    echo Branch is OK with naming conventions
+else
+    echo Your branch \'$branch\' needs a better name to match conventions. Sorry.
+    echo This is the regex that rules branch naming: $valid
+    echo You can try a few at https://regex101.com/
+    echo Then, please, follow these steps: https://www.w3docs.com/snippets/git/how-to-rename-git-local-and-remote-branches.html
+    exit 1
+fi
